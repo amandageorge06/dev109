0a3375abbd8099d2b7ac6ebd929318c72752de1c
amandageorge06 <amandalgeorgex@gmail.com>
2024-11-02 13:59:52 -0700

    Create java.js

---
+++ a3/java.js
@@ -0,0 +1,66 @@
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+}
+
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = 0; i < pHeight; i++) {
+        rLine += "<p>";
+        for (j = 0; j <= i; j++) {
+            if (j % 2) 
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else 
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "</p>";
+    }
+    document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = pHeight; i > 0; i--) {
+        rLine += "<p>";
+        for (j = 0; j < i; j++) {
+            if (j % 2) 
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else 
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "</p>";
+    }
+    document.getElementById("downRight").innerHTML = rLine;
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = 0; i < pHeight; i++) {
+        rLine += "<p>";
+        for (j = 0; j < (pHeight - i); j++) { // Adjusting to create the left side
+            if (j % 2) 
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else 
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "</p>";
+    }
+    document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var rLine = "";
+    for (i = pHeight; i > 0; i--) {
+        rLine += "<p>";
+        for (j = 0; j < (pHeight - i + 1); j++) { // Adjusting to create the left side
+            if (j % 2) 
+                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+            else 
+                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+        }
+        rLine += "</p>";
+    }
+    document.getElementById("downLeft").innerHTML = rLine;
+}
