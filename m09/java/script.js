1d8fd2e7900b2c03599867b9a318b439742fa4f1
amandageorge06 <amandalgeorgex@gmail.com>
2024-11-30 23:21:43 -0800

    Update script.js

--- m09/java/script.js
+++ m09/java/script.js
@@ -1,19 +1,18 @@
-var  mybutton = document.querySelector("button");
-mybutton.addEventListener("click", function(event) {
+    var myButton = document.querySelector("#clear-button");
 
+    myButton.addEventListener("click", function(event) {
+      var elements = document.getElementsByClassName("dot");
+      for (let index = elements.length - 1; index >= 0; index--) {
+        elements[index].parentNode.removeChild(elements[index]);
+      }
 
-var element = document.getElementsByTagName("div");
-for (index = element.length - 1; index >= 0; index--) {
-    element[index].parentNode.removeChild(element[index]);
-}
+      event.stopPropagation();
+    });
 
-
-event.stopPropagation();
-  });
-  addEventListener("click", function(event) {
-    var dot = document.createElement("div");
-    dot.className = "dot";
-    dot.style.left = (event.pageX - 4) + "px";
-    dot.style.top = (event.pageY - 4) + "px";
-    document.body.appendChild(dot);
-  });
+    addEventListener("click", function(event) {
+      var dot = document.createElement("div");
+      dot.className = "dot";
+      dot.style.left = (event.pageX - 4) + "px";
+      dot.style.top = (event.pageY - 4) + "px";
+      document.body.appendChild(dot);
+    });
